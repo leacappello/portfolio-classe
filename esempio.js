1e14850c3fedbd2cbe96a7e629fc4c2bdcf1a42c
leacappello <81253331+leacappello@users.noreply.github.com>
2025-07-21 09:45:54 +0200

    Primo commit

---
+++ esempio.js
@@ -0,0 +1,10 @@
+document.getElementById("cambia").addEventListener("click", function() {
+    document.getElementById("messaggio").textContent ="Hai cambiato il testo!";
+});
+
+input.addEventListener("keyup", function(event) {
+    console.log("Hai scritto: ", event.key);
+
+});
+
+card.addEventListener("mouseenter" , fun)
